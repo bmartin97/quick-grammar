f7bc898aba1cabcc94c43c82fce1cb7456a83017
Adriano Kovacs <86926838+drianoK@users.noreply.github.com>
2023-10-10 14:28:03 +0200

    create a new component, add new card colors

--- src/components/Helper.js
+++ src/components/Helper.js
@@ -1,7 +1,9 @@
 export const CARD_COLORS = {
   orange: 'orange',
   blue: 'blue',
-  yellow: 'yellow'
+  yellow: 'yellow',
+  magenta: 'magenta',
+  green: 'green'
 };
 
 export const DROPAREA_THEMES = {
